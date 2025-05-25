df8c8a14bbe69067ee00c4226b58fb158bc2a5b8
Bigbadbilal <bilalnaruto123@gmail.com>
2025-05-25 02:17:38 -0400

    stage

--- weather-dashboard/vite.config.ts
+++ weather-dashboard/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
 })
